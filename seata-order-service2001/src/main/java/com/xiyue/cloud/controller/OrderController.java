5333b769f20baeb1d750263a18a46e0e2cef120f
tirkl <tirklee@hotmail.com>
2020-10-23 22:33:07 +0800

    SpringCloud Alibaba Seata处理分布式事务

--- seata-order-service2001/src/main/java/com/xiyue/cloud/controller/OrderController.java
+++ seata-order-service2001/src/main/java/com/xiyue/cloud/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.xiyue.cloud.domain.CommonResult;
 import com.xiyue.cloud.domain.Order;
 import com.xiyue.cloud.service.OrderService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -17,9 +18,9 @@ public class OrderController{
 
 
     @GetMapping("/order/create")
-    public CommonResult create(Order order)
+    public CommonResult create(@RequestBody  Order order)
     {
-        orderService.create(order);
+         orderService.create(order);
         return new CommonResult(200,"订单创建成功");
     }
 }
